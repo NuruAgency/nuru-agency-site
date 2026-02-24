24bc701c1def1e431c8d5e57a005801c86ea3722
Nuru Agency <benjamin.trazie@nuruagency.com>
2026-02-24 06:17:00 +0000

    Update HeroSection.tsx

--- src/components/pages/landing/components/HeroSection.tsx
+++ src/components/pages/landing/components/HeroSection.tsx
@@ -38,13 +38,27 @@ export function HeroSection() {
   return (
     <section className="relative min-h-screen flex flex-col justify-center items-center pt-32 pb-20 overflow-hidden">
       <div className="absolute inset-0 z-0 opacity-70">
-        <LiquidEther 
-          // J'utilise les couleurs de ta charte : Primary (Magenta), Blue, Purple
-          colors={['#E60C73', '#397BBF', '#4E4595']} 
-          mouseForce={30} // Force de l'interaction avec la souris
-          cursorSize={120} // Taille du curseur fluide
-          autoSpeed={0.8} // Vitesse de l'animation automatique
-        />
+  <LiquidEther
+    colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+    mouseForce={20}
+    cursorSize={100}
+    isViscous
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={false}
+    autoDemo
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6}
+    color0="#5227FF"
+    color1="#FF9FFC"
+    color2="#B19EEF"
+/>
+
       </div>
 
       <motion.div 
