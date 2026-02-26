3e9621b45430fb413e10abb23c8ca25ab0a075fd
Nuru Agency <benjamin.trazie@nuruagency.com>
2026-02-26 09:51:32 +0000

    he

--- src/app/realisations/page.tsx
+++ src/app/realisations/page.tsx
@@ -1,4 +1,4 @@
-import { PortfolioHero } from "@/components/pages/realisations/PortfolioHero";
+// import { PortfolioHero } from "@/components/pages/realisations/PortfolioHero";
 import { ProjectShowcase } from "@/components/pages/projects/ProjectShowcase";
 import { ProjectArchives } from "@/components/pages/projects/ProjectArchives";
 import { PortfolioCTA } from "@/components/pages/realisations/PortfolioCTA";
@@ -6,7 +6,7 @@ import { PortfolioCTA } from "@/components/pages/realisations/PortfolioCTA";
 
 export default function Home() {
     return ( <>
-       <PortfolioHero />
+       {/* <PortfolioHero /> */}
        <ProjectShowcase />
        <ProjectArchives />
        <PortfolioCTA />
